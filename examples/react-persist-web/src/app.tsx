fff6d1575351e8fb7f2fd7cb41b198e065a020ce
Aleksey Chendemerov <chendemerov.aleksey@gmail.com>
2025-02-04 15:41:29 +0300

    docs(persist-web-storage): update persist-web-storage docs and example

--- examples/react-persist-web/src/app.tsx
+++ examples/react-persist-web/src/app.tsx
@@ -1,42 +1,26 @@
-import { atom, onConnect } from '@reatom/framework'
-import {
-  withLocalStorage,
-  withBroadcastChannel,
-  withSessionStorage,
-  withIndexedDb,
-} from '@reatom/persist-web-storage'
+import { atom } from '@reatom/framework'
+import { withLocalStorage, withBroadcastChannel, withSessionStorage, withIndexedDb } from '@reatom/persist-web-storage'
 import { useAtom } from '@reatom/npm-react'
 
 const inputWithLocalStorageAtom = atom('', 'inputWithLocalStorageAtom').pipe(
   withLocalStorage('inputWithLocalStorageAtom'),
 )
 
-const inputWithSessionStorageAtom = atom(
-  '',
-  'inputWithSessionStorageAtom',
-).pipe(withSessionStorage('inputWithSessionStorageAtom'))
-
-const inputWithBroadcastChannelAtom = atom(
-  '',
-  'inputWithBroadcastChannelAtom',
-).pipe(withBroadcastChannel('inputWithBroadcastChannelAtom'))
-
-const inputIndexedDbAtom = atom('', 'inputIndexedDbAtom').pipe(
-  withIndexedDb('inputIndexedDbAtom'),
+const inputWithSessionStorageAtom = atom('', 'inputWithSessionStorageAtom').pipe(
+  withSessionStorage('inputWithSessionStorageAtom'),
 )
-const inputIndexedDbReadyAtom = atom(false, 'inputIndexedDbReadyAtom')
-onConnect(inputIndexedDbAtom, async (ctx) => {
-  await ctx.schedule(() => new Promise((r) => requestIdleCallback(r)))
-  inputIndexedDbReadyAtom(ctx, true)
-  return () => inputIndexedDbReadyAtom(ctx, false)
-})
+
+const inputWithBroadcastChannelAtom = atom('', 'inputWithBroadcastChannelAtom').pipe(
+  withBroadcastChannel('inputWithBroadcastChannelAtom'),
+)
+
+const inputIndexedDbAtom = atom('', 'inputIndexedDbAtom').pipe(withIndexedDb('inputIndexedDbAtom'))
 
 export const App = () => {
   const [searchLS, setSearchLS] = useAtom(inputWithLocalStorageAtom)
   const [searchSS, setSearchSS] = useAtom(inputWithSessionStorageAtom)
   const [searchBC, setSearchBC] = useAtom(inputWithBroadcastChannelAtom)
   const [searchIDB, setSearchIDB] = useAtom(inputIndexedDbAtom)
-  const [inputIndexedDbReady] = useAtom(inputIndexedDbReadyAtom)
 
   return (
     <main>
@@ -44,42 +28,26 @@ export const App = () => {
       <p>
         <label>
           I'm synced using localStorage
-          <input
-            value={searchLS}
-            onChange={(e) => setSearchLS(e.currentTarget.value)}
-          />
+          <input value={searchLS} onChange={(e) => setSearchLS(e.currentTarget.value)} />
         </label>
       </p>
       <p>
         <label>
           I'm not synced because I use sessionStorage
-          <input
-            value={searchSS}
-            onChange={(e) => setSearchSS(e.currentTarget.value)}
-          />
+          <input value={searchSS} onChange={(e) => setSearchSS(e.currentTarget.value)} />
         </label>
       </p>
       <p>
         <label>
           I'm synced using BroadcastChannel
-          <input
-            value={searchBC}
-            onChange={(e) => setSearchBC(e.currentTarget.value)}
-          />
+          <input value={searchBC} onChange={(e) => setSearchBC(e.currentTarget.value)} />
         </label>
       </p>
       <p>
-        {inputIndexedDbReady ? (
-          <label>
-            I'm synced using IndexedDB
-            <input
-              value={searchIDB}
-              onChange={(e) => setSearchIDB(e.currentTarget.value)}
-            />
-          </label>
-        ) : (
-          'IndexedDB initialization...'
-        )}
+        <label>
+          I'm synced using IndexedDB
+          <input value={searchIDB} onChange={(e) => setSearchIDB(e.currentTarget.value)} />
+        </label>
       </p>
     </main>
   )
--- examples/react-persist-web/src/main.tsx
+++ examples/react-persist-web/src/main.tsx
@@ -1,18 +1,18 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { createCtx, connectLogger } from '@reatom/framework'
 import { reatomContext } from '@reatom/npm-react'
+import { withIndexedDb } from '../../../packages/persist-web-storage/src/reatomPersistIndexedDb'
 import { App } from './app'
 
 const ctx = createCtx()
 
 connectLogger(ctx)
 
+await withIndexedDb.init(ctx)
+
 const root = ReactDOM.createRoot(document.getElementById('root')!)
 root.render(
-  <React.StrictMode>
-    <reatomContext.Provider value={ctx}>
-      <App />
-    </reatomContext.Provider>
-  </React.StrictMode>,
+  <reatomContext.Provider value={ctx}>
+    <App />
+  </reatomContext.Provider>,
 )
